686d10ed9075b6f9da1212019c0492fb56960d3f
vpoturaev <viacheslav.poturaev@lazada.com>
2016-06-25 16:00:05 +0700

    initial db renderer

---
+++ src/Command/Migrate.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Helloelo\Command;
+
+
+use Helloelo\Entity\User;
+use Yaoi\Command;
+use Yaoi\Command\Definition;
+use Yaoi\Database\Definition\Table;
+use Yaoi\Log;
+
+class Migrate extends Command
+{
+    public $wipe;
+    public $dryRun;
+
+    static function setUpDefinition(Definition $definition, $options)
+    {
+        $options->wipe = Command\Option::create()->setDescription('Recreate tables');
+        $options->dryRun = Command\Option::create()->setDescription('Read-only mode');
+        $definition->name = 'migrate';
+        $definition->description = 'Actualize application data schema';
+    }
+
+    public function performAction()
+    {
+        /** @var Table[] $tables */
+        $tables = array(
+            User::table(),
+        );
+
+        $log = new Log('colored-stdout');
+        if ($this->wipe) {
+            foreach ($tables as $table) {
+                $table->migration()->setLog($log)->setDryRun($this->dryRun)->rollback();
+            }
+        }
+        foreach ($tables as $table) {
+            $table->migration()->setLog($log)->setDryRun($this->dryRun)->apply();
+        }
+    }
+
+
+}
---
+++ src/Tool.php
@@ -0,0 +1,23 @@
+<?php
+namespace Helloelo;
+
+use Helloelo\Command\Migrate;
+use Yaoi\Command\Application;
+use Yaoi\Command\Definition;
+
+class Tool extends Application
+{
+    public $migrate;
+
+    /**
+     * @param Definition $definition
+     * @param \stdClass|static $commandDefinitions
+     */
+    static function setUpCommands(Definition $definition, $commandDefinitions)
+    {
+        $commandDefinitions->migrate = Migrate::definition();
+        $definition->name = 'le-tool';
+    }
+
+
+}
