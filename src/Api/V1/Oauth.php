c5fda4cae840bd20dadac5089207fbb8df6c6e8e
vpoturaev <viacheslav.poturaev@lazada.com>
2016-06-26 03:03:09 +0700

    oauth2

--- src/Api/V1/Oauth.php
+++ src/Api/V1/Oauth.php
@@ -7,12 +7,50 @@ use Yaoi\Command\Definition;
 
 class Oauth extends Command
 {
+    public static $clientId;
+    public static $clientSecret;
+    public static $redirectUrl;
+
     static function setUpDefinition(Definition $definition, $options)
     {
     }
 
     public function performAction()
     {
+        if (isset($_GET['code'])) {
+            //$url = 'https://accounts.google.com/o/oauth2/token';
+            $url = 'https://www.googleapis.com/oauth2/v4/token';
+
+            /*
+             * POST /oauth2/v4/token HTTP/1.1
+Host: www.googleapis.com
+Content-Type: application/x-www-form-urlencoded
+
+code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+client_id=8819981768.apps.googleusercontent.com&
+client_secret={client_secret}&
+redirect_uri=https://oauth2-login-demo.appspot.com/code&
+grant_type=authorization_code
+             */
+            $post = array(
+                'code' => $_GET['code'],
+                'client_id' => self::$clientId,
+                'client_secret' => self::$clientSecret,
+                'redirect_url' => self::$redirectUrl,
+                'grant_type' => 'authorization_code',
+            );
+            $opts = array('http' =>
+                array(
+                    'method' => 'POST',
+                    'header' => 'Content-type: application/x-www-form-urlencoded',
+                    'content' => http_build_query($post)
+                )
+            );
+            $context = stream_context_create($opts);
+            $result = file_get_contents($url, false, $context);
+            return $result;
+        }
+
         return array('bich' => 'one', 'get' => $_GET);
     }
 
